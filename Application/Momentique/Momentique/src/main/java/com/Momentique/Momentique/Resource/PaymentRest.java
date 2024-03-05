725f401da5e1035c8f4dc06bde690a4fcf7e94f1
tegmen88 <cite7173@gmail.com>
2024-03-05 12:26:55 +0100

    strip change

--- Application/Momentique/Momentique/src/main/java/com/Momentique/Momentique/Resource/PaymentRest.java
+++ Application/Momentique/Momentique/src/main/java/com/Momentique/Momentique/Resource/PaymentRest.java
@@ -3,20 +3,22 @@ package com.Momentique.Momentique.Resource;
 import com.Momentique.Momentique.Services.StripeService;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PaymentController {
+public class PaymentRest {
 
     @Autowired
     private StripeService stripeService;
 
     @PostMapping("/create-payment-intent")
-    public PaymentIntent createPaymentIntent() throws Exception {
-
-        return stripeService.createPaymentIntent(1000, "usd"); // Exempelbelopp och valuta
+    public String createPaymentIntent() throws Exception {
+        PaymentIntent paymentIntent = stripeService.createPaymentIntent(1000, "usd");
+        return paymentIntent.getClientSecret();
     }
 }
 
