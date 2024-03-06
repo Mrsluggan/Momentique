28aebdd888ba0dbfc82c859c9906f8b4524ff6c2
tegmen88 <cite7173@gmail.com>
2024-03-06 10:17:29 +0100

    checkout session tillagd, autentiseringen fungerar

--- Application/Momentique/Momentique/src/main/java/com/Momentique/Momentique/Resource/PaymentRest.java
+++ Application/Momentique/Momentique/src/main/java/com/Momentique/Momentique/Resource/PaymentRest.java
@@ -9,16 +9,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stripe.model.checkout.Session;
+
 @RestController
 public class PaymentRest {
 
     @Autowired
     private StripeService stripeService;
 
+    // Endpoint payment intent 
     @PostMapping("/create-payment-intent")
     public String createPaymentIntent() throws Exception {
-        PaymentIntent paymentIntent = stripeService.createPaymentIntent(1000, "usd");
+        PaymentIntent paymentIntent = stripeService.createPaymentIntent(1000, "sek");
         return paymentIntent.getClientSecret();
+    }
+
+    // Endpoint checkout Session
+    @PostMapping("/create-checkout-session")
+    public String createCheckoutSession() throws Exception {
+        Session session = stripeService.createCheckoutSession();
+        return session.getUrl(); // Returnerar URL till Checkout Session för omdirigering
     }
 }
 
--- Application/Momentique/Momentique/src/main/java/com/Momentique/Momentique/Services/StripeService.java
+++ Application/Momentique/Momentique/src/main/java/com/Momentique/Momentique/Services/StripeService.java
@@ -6,6 +6,9 @@ import com.stripe.param.PaymentIntentCreateParams;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+
 @Service
 public class StripeService {
 
@@ -23,5 +26,33 @@ public class StripeService {
             .build();
 
         return PaymentIntent.create(params);
+    }
+
+    // Checkout Session
+    public Session createCheckoutSession() throws Exception {
+        SessionCreateParams params = SessionCreateParams.builder()
+            .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+            .setMode(SessionCreateParams.Mode.PAYMENT)
+            .setSuccessUrl("https://example.com/success")
+            .setCancelUrl("https://example.com/cancel")
+            .addLineItem(
+                SessionCreateParams.LineItem.builder()
+                    .setQuantity(1L)
+                    .setPriceData(
+                        SessionCreateParams.LineItem.PriceData.builder()
+                            .setCurrency("sek")
+                            .setUnitAmount(1000L) 
+                            .setProductData(
+                                SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                    .setName("Produktens namn här")
+                                    .build()
+                            )
+                            .build()
+                    )
+                    .build()
+            )
+            .build();
+
+        return Session.create(params);
     }
 }
